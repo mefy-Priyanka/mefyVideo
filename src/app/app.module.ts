7259ecaf4ed820c7ac032f39f8e051056c6b8d6f
mefy-Priyanka <priyanka.saha@mefy.care>
2018-12-07 16:05:59 +0530

    login dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const approutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(approutes), 
     AppRoutingModule
   ],
   providers: [],
